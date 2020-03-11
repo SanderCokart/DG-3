3a432681b2d7ffb940f6f7f1fcac8ef33ca5606f
Sander Cokart <cokart32@gmail.com>
2020-03-11 10:14:16 +0100

    initial commit eindopdracht

---
+++ eindopdracht/assets/js/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
