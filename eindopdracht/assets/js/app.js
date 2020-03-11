05256714c723d9539d201b4298be7da627844542
Sander Cokart <cokart32@gmail.com>
2020-03-11 10:41:00 +0100

    initialized a CSS baseline with MUI

--- eindopdracht/assets/js/app.js
+++ eindopdracht/assets/js/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CssBaseline} from "@material-ui/core";
 
 export default function App() {
     return (
         <div>
-            Hello World!
+            <CssBaseline>
+
+            </CssBaseline>
         </div>
     )
 }
