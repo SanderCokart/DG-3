009c3bfb491ebe2e15ed5ad5016c61ac7544d78d
Sander Cokart <cokart32@gmail.com>
2020-03-11 10:39:42 +0100

    person entity added with 4 columns: id, fName, lName and age.

---
+++ eindopdracht/src/Entity/Person.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonRepository")
+ */
+class Person
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFName(): ?string
+    {
+        return $this->fName;
+    }
+
+    public function setFName(string $fName): self
+    {
+        $this->fName = $fName;
+
+        return $this;
+    }
+
+    public function getLName(): ?string
+    {
+        return $this->lName;
+    }
+
+    public function setLName(string $lName): self
+    {
+        $this->lName = $lName;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+}
