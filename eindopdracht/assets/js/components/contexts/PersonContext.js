b8c8fd459d4f98f37fe32308c7b13d01fbef750c
SanderCokartSchool <112039@student.drenthecollege.nl>
2020-03-11 20:21:21 +0100

    added create function in PersonContext.js

--- eindopdracht/assets/js/components/contexts/PersonContext.js
+++ eindopdracht/assets/js/components/contexts/PersonContext.js
@@ -1,4 +1,5 @@
 import React, {Component, createContext} from 'react';
+import axios from 'axios';
 
 export const PersonContext = createContext();
 
@@ -10,8 +11,40 @@ class PersonContextProvider extends Component {
         };
     }
 
-    //create
+    /**
+     * Create a new person.
+     * @param {object} event - contains all events upon function
+     * @param {object} person - contains data of the created person
+     * @param {string} person.fName - persons' first name
+     * @param {string} person.lName - persons' last name
+     * @param {int} person.age - persons' age
+     */
+    createPerson(event, person) {
+        //prevent form from refreshing the page
+        event.preventDefault();
 
+        //post data to route and send the person
+        axios.post('/api/person/create', person)
+            .then(response => {
+                //copy people array
+                let people = [...this.state.people];
+
+                //push new person received from server to array
+                data.push(response.data.person);
+
+                //overwrite array
+                this.setState({
+                    people: people,
+                })
+            })
+
+            //catch any errors
+            .catch(error => {
+
+                //log error
+                console.error(error);
+            })
+    }
 
     //read
 
