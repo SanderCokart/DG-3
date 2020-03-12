8ed9e08ae6d542566962cd44ea3d9f0779b3a4ca
Sander Cokart <cokart32@gmail.com>
2020-03-12 09:23:18 +0100

    added delete function to PersonContext.js

--- eindopdracht/assets/js/components/contexts/PersonContext.js
+++ eindopdracht/assets/js/components/contexts/PersonContext.js
@@ -1,4 +1,5 @@
 import React, {Component, createContext} from 'react';
+import {AxiosInstance as axios} from "axios";
 
 export const PersonContext = createContext();
 
@@ -19,7 +20,36 @@ class PersonContextProvider extends Component {
     //update
 
 
-    //delete
+    /**
+     *
+     * @param {object} personToDelete - contains data of the created person
+     * @param {int} person.id - persons' id
+     * @param {string} person.fName - persons' first name
+     * @param {string} person.lName - persons' last name
+     * @param {int} person.age - persons' age
+     */
+    deletePerson(personToDelete) {
+        axios.delete('/api/todo/delete/' + personToDelete.id)
+            .then(response => {
+                //create copy current state
+                let people = [...this.state.people];
+
+                //extract the user that matches the id
+                let person = people.find(person => {
+                    return person.id === personToDelete.id
+                });
+
+                //delete the person from people who's index matches that with the extracted person
+                people.splice(people.indexOf(person), 1);
+
+                //overwrite people with new array without the person that had to be deleted
+                this.setState({
+                    people: people,
+                });
+            }).catch(error => {
+            console.error(error);
+        })
+    }
 
 
     //render
