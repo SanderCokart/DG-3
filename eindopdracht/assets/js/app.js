dc62ae45f96e6fdec91db0dbc4a2c38a8e9f922e
SanderCokartSchool <122039@student.drenthecollege.nl>
2020-03-19 16:33:53 +0100

    changed file location and added PersonTable.js to app.js

--- eindopdracht/assets/js/app.js
+++ eindopdracht/assets/js/app.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {CssBaseline} from "@material-ui/core";
-import PersonContext from "./components/contexts/PersonContext";
+import PersonContext from './components/api/PersonContext';
+import PersonTable from './components/react/PersonTable';
 
 export default function App() {
     return (
         <div>
             <CssBaseline>
                 <PersonContext>
-
+                    <PersonTable/>
                 </PersonContext>
             </CssBaseline>
         </div>
